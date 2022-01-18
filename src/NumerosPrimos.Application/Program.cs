76732c83e61e82bc9dc5b62dc4b5ba142827da11
tiago.rossetto@logicalminds.com.br <tiago.rossetto@logicalminds.com.br>
2022-01-18 18:48:11 -0300

    Init

---
+++ src/NumerosPrimos.Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NumerosPrimos.Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
